511c94edce991b22aa064f88dcb4f819c6813799
Karim Ghorab <karim.ghorab@hotmail.com>
2023-03-28 18:36:40 +0200

    Initial Commit

---
+++ Git Started/Git_StartedApp.swift
@@ -0,0 +1,17 @@
+//
+//  Git_StartedApp.swift
+//  Git Started
+//
+//  Created by Karim Ghorab on 28.03.23.
+//
+
+import SwiftUI
+
+@main
+struct Git_StartedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
